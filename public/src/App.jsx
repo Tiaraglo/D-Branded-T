b23818308337f1cacb161b4cc32dead608d22ecc
Tiaraglo <siwutiara@gmail.com>
2024-07-08 08:22:02 +0700

    final touch

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -4,20 +4,6 @@ import router from './routers'
 import { RouterProvider } from 'react-router-dom'
 
 
-
-// export default function App() {
-//     const url = 'https://phase2-aio.vercel.app'
-//     const [page, setPage] = useState('home')
-
-//     return (
-//         <>
-//             <Nav setPage={setPage} />
-//             <br />
-//             <HomePage url={url} />
-//         </>
-//     )
-// }
-
 export default function App() {
 
     return (
